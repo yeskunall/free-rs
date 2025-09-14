e3181eee224be8233e33329100758d8fac8c2264
Kunall Banerjee <hey@kimchiii.space>
2025-09-14 01:22:19 -0400

    Get usage statistics

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,32 @@
-use libc::{c_int, size_t, sysctl};
+use libc::sysctl;
 
-/// Returns a Mach system’s virtual memory page size in bytes.
-///
-/// Your architecture type determines your page size. For example, on Apple Silicon chips the default page size is 16KB.
-pub fn get_page_size() -> usize {
-    let mut page_size: size_t = 0;
-    let mut size = std::mem::size_of::<size_t>();
+/// Get usage statistics.
+pub fn get_usage_statistics() -> Result<(u64, u64), Box<dyn std::error::Error>> {
+    let usage: u64;
+    let mut total: u64 = 0;
 
     unsafe {
-        let mib: [c_int; 2] = [libc::CTL_HW, libc::HW_PAGESIZE];
-        sysctl(
-            mib.as_ptr() as *mut c_int,
-            2,
-            &mut page_size as *mut _ as *mut _,
+        let mut mib = [libc::CTL_HW, libc::HW_MEMSIZE];
+        let mut size = std::mem::size_of::<u64>();
+
+        let kern_ret = sysctl(
+            mib.as_mut_ptr(),
+            mib.len() as _,
+            &mut total as *mut _ as *mut _,
             &mut size,
             std::ptr::null_mut(),
             0,
         );
+
+        if kern_ret != 0 {
+            return Err("failed to get total memory".into());
+        }
     }
 
-    page_size
-}
-
-/// Get virtual memory statistics.
-///
-///
-pub fn get_vm_statistics() -> libc::vm_statistics64 {
-    let mut count: u32 = libc::HOST_VM_INFO64_COUNT;
-
     unsafe {
-        let mut stats = std::mem::MaybeUninit::<libc::vm_statistics64>::uninit();
+        let mut count = libc::HOST_VM_INFO64_COUNT;
+        let page_size = libc::sysconf(libc::_SC_PAGESIZE) as u64;
+        let mut stats = std::mem::zeroed::<libc::vm_statistics64>();
 
         let kern_ret = libc::host_statistics64(
             #[allow(deprecated)]
@@ -40,9 +37,18 @@ pub fn get_vm_statistics() -> libc::vm_statistics64 {
         );
 
         if kern_ret != 0 {
-            panic!("host_statistics64 failed with kern_return_t = {}", kern_ret);
+            return Err("failed to get memory stats".into());
         }
 
-        stats.assume_init()
+        usage = (stats.active_count as u64
+            + stats.inactive_count as u64
+            + stats.wire_count as u64
+            + stats.speculative_count as u64
+            + stats.compressor_page_count as u64
+            - stats.purgeable_count as u64
+            - stats.external_page_count as u64)
+            * page_size;
     }
+
+    Ok((total, usage))
 }
